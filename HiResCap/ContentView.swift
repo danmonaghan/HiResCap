3ba946a1a2b5ce946fd0968c3cae9f189cf338fa
dan zugazi <dan@zugazi.com>
2025-04-25 00:09:38 -0400

    Add depth map capture

--- HiResCap/ContentView.swift
+++ HiResCap/ContentView.swift
@@ -19,7 +19,7 @@ struct ContentView: View {
                 .edgesIgnoringSafeArea(.all)
 
             FrameCaptureOverlayView(captureModel: captureModel)
-                .frame(width: 160, height: 90)
+                .frame(width: 160, height: 120)
                 .cornerRadius(8)
                 .padding()
                 .background(.ultraThinMaterial)
@@ -27,6 +27,13 @@ struct ContentView: View {
                 .padding([.top, .trailing], 20)
                 .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
 
+            DepthCaptureView(model: captureModel)
+                .frame(width: 160, height: 120)
+                .padding()
+                .background(.ultraThinMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomTrailing)
+            
             VStack {
                 Spacer()
                 Button("Capture Hi-Res Frame") {
@@ -48,6 +55,7 @@ struct ARViewContainer: UIViewRepresentable {
 
         let config = ARWorldTrackingConfiguration()
         config.planeDetection = [.horizontal, .vertical]
+        config.frameSemantics = .sceneDepth
         config.environmentTexturing = .automatic
 
         if let bestFormat = ARWorldTrackingConfiguration.supportedVideoFormats.first(where: {
@@ -82,7 +90,22 @@ extension Notification.Name {
 
 class FrameCaptureModel: ObservableObject {
     @Published var capturedImage: UIImage?
+    @Published var capturedDepth: UIImage?
     @Published var resolutionText: String = ""
+    
+    private var rotationAngle: Double {
+            guard let ori = arView?
+                    .window?
+                    .windowScene?
+                    .interfaceOrientation
+            else { return 0 }
+            switch ori {
+            case .landscapeLeft:       return 180
+            case .portrait:            return  90
+            case .portraitUpsideDown:  return -90
+            default:                   return   0
+            }
+        }
     
     weak var arView: ARView?
     
@@ -91,24 +114,32 @@ class FrameCaptureModel: ObservableObject {
     }
     
     public func getHiresFrame(isJpegQuality: Bool = true) {
-        arView?.session.captureHighResolutionFrame { [weak self] (frame, error) in
-            guard let self = self, let frame = frame else { return }
-            
-            let pixelBuffer = frame.capturedImage
-            let ciImage = CIImage(cvPixelBuffer: pixelBuffer)
-            let context = CIContext()
-            
-            guard let cgImage = context.createCGImage(ciImage, from: ciImage.extent) else { return }
-            let uiImage = UIImage(cgImage: cgImage)
-            
-            print("Captured resolution: \(cgImage.width) × \(cgImage.height)")
-            
-            DispatchQueue.main.async {
-                self.capturedImage = uiImage
-                self.resolutionText = "\(cgImage.width) × \(cgImage.height)"
+            arView?.session.captureHighResolutionFrame { [weak self] (frame, error) in
+                guard let self = self, let frame = frame else { return }
+
+                // 1) color image
+                let colorBuffer = frame.capturedImage
+                let ciColor   = CIImage(cvPixelBuffer: colorBuffer)
+                let context   = CIContext()
+                guard let cgColor = context.createCGImage(ciColor, from: ciColor.extent) else { return }
+                let uiColor = UIImage(cgImage: cgColor)
+
+                // 2) depth image
+                guard let liveFrame = self.arView?.session.currentFrame,
+                      let depthData = liveFrame.sceneDepth else { return }
+                let depthBuffer = depthData.depthMap
+                let ciDepth = CIImage(cvPixelBuffer: depthData.depthMap)
+                guard let cgDepth = context.createCGImage(ciDepth, from: ciDepth.extent) else { return }
+                let uiDepthUnrotated = UIImage(cgImage: cgDepth)
+                let uiDepth = uiDepthUnrotated.rotated(by: CGFloat(self.rotationAngle))
+                
+                DispatchQueue.main.async {
+                    self.capturedImage = uiColor
+                    self.capturedDepth = uiDepth
+                    self.resolutionText = "\(cgColor.width) × \(cgColor.height)"
+                }
             }
         }
-    }
 }
 
 struct FrameCaptureView: View {
@@ -141,6 +172,27 @@ struct FrameCaptureView: View {
     }
 }
 
+struct DepthCaptureView: View {
+    @ObservedObject var model: FrameCaptureModel
+
+    var body: some View {
+        Group {
+            if let depth = model.capturedDepth {
+                Image(uiImage: depth)
+                    .resizable()
+                    .scaledToFit()
+                    .border(Color.white, width: 1)
+            } else {
+                Text("No depth yet")
+                    .foregroundColor(.gray)
+            }
+        }
+        .frame(width: 180, height: 180)
+        .background(Color.black.opacity(0.2))
+        .cornerRadius(8)
+    }
+}
+
 struct FrameCaptureOverlayView: View {
     @ObservedObject var captureModel: FrameCaptureModel
 
@@ -156,6 +208,29 @@ struct FrameCaptureOverlayView: View {
     }
 }
 
-#Preview {
-    ContentView()
+extension UIImage {
+    func rotated(by degrees: CGFloat) -> UIImage {
+        let radians = degrees * .pi/180
+        // figure out the size of the rotated view’s containing box
+        var newSize = CGRect(origin: .zero, size: size)
+            .applying(CGAffineTransform(rotationAngle: radians))
+            .integral.size
+
+        UIGraphicsBeginImageContextWithOptions(newSize, false, scale)
+        guard let ctx = UIGraphicsGetCurrentContext() else { return self }
+
+        // move origin to middle so we rotate around the center
+        ctx.translateBy(x: newSize.width/2, y: newSize.height/2)
+        ctx.rotate(by: radians)
+
+        // draw the image into the context
+        draw(in: CGRect(x: -size.width/2,
+                        y: -size.height/2,
+                        width: size.width,
+                        height: size.height))
+
+        let rotated = UIGraphicsGetImageFromCurrentImageContext() ?? self
+        UIGraphicsEndImageContext()
+        return rotated
+    }
 }
